bea3b8ff7790c2c001543a598db6ef73b5c185e5
yuchigao <yuchigao@foxmail.com>
2026-02-17 20:27:18 +0800

    add STATIC_ROOT for collectstatic

--- backend/school_system/settings.py
+++ backend/school_system/settings.py
@@ -100,6 +100,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    # Add other static files here if any
+]
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
